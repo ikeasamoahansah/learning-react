0d7a64b0a693d02f656365ed49cb25b795a8c838
Ike <dylanraspino264@gmail.com>
2023-09-14 20:58:41 +0000

    same

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 // function Button(props) {
@@ -15,30 +13,43 @@ import './App.css'
 //   )
 // }
 
-const COLORS = ["green", "pink", "blue", "white", "black", "grey"]
+// const COLORS = ["green", "pink", "blue", "white", "black", "grey"]
+
+function CustomInput() {
+  const [value, setValue] = useState('');
+
+  return (
+
+    <>
+    <h1>{value}</h1>
+    <input
+      placeholder='first name'
+      type="text"
+      value={value}
+      onChange={(e) => setValue(e.target.value)} />
+    </>
+  );
+}
 
 function App() {
-  const [backgroundColor, setBackgroundColor] = useState(COLORS[0])
+  // const [backgroundColor, setBackgroundColor] = useState(COLORS[0])
   // const animals = ["Cow", "Goat", "Sheep", "Pig"];
 
   // const handleButtonClick = () => {
   //   window.location.href = 'http://www.google.com';
   // };
 
-  const onButtonClick = (color) => () => {
-    setBackgroundColor(color);
-  }
+  // const onButtonClick = (color) => () => {
+  //   setBackgroundColor(color);
+  // }
 
   return (
-      <div
-      className='App'
-      style={{backgroundColor}}
-      >
+      <div>
         {/* <h1>Props: </h1>
         <Button text="Don't click me" color="red" fontSize={12} handleClick={handleButtonClick} />
         <Button text="Click Me" color="green" fontSize={12} /> */}
 
-        {COLORS.map((color) => (
+        {/* {COLORS.map((color) => (
           <button
           type="button"
           key={color}
@@ -47,7 +58,8 @@ function App() {
           >
             {color}
           </button>
-        ))}
+        ))} */}
+        <CustomInput/>
       </div>
   )
 }
